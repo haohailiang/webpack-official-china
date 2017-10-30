f94a75c5671913ab9454bd23a34b157139f17d0c
haohailiang <646263409@qq.com>
2017-10-30 16:58:14 +0800

    加载行内CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 const path = require('path');
+const srcPath = path.resolve(__dirname, 'src');
 
 module.exports = {
   entry: './src/js/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ],
+        include: srcPath
+      }
+    ]
   }
 }; 
