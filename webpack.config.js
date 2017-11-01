0f818fdc970ac0a6a33914704e4dea1ddcf73cfb
haohailiang <646263409@qq.com>
2017-11-01 17:02:10 +0800

    简单的webpack.config.js文件

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: '最简单的配置'
+    })
+  ],
+};
