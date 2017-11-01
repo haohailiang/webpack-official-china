7f684a9c2880a3074392741824365bb7d0482fa8
haohailiang <646263409@qq.com>
2017-11-01 10:30:40 +0800

    export default-module.exports区别
    export default是ES2015的模块规范
    module.exports是CommonJS的模块规范

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,31 @@
 function createComponent(){
-	var element = document.createElement('button');
-	element.innerHTML = 'click me';
-	element.onclick = () => {
+	var btn1 = document.createElement('button');
+	var btn2 = document.createElement('button');
+	var btn3 = document.createElement('button');
+	btn1.innerHTML = 'button1';
+	btn2.innerHTML = 'button2';
+	btn3.innerHTML = 'button3';
+	btn1.onclick = () => {
 		import(/* webpackChunkName: "global" */ './global.js').then( sayHello => {
-			sayHello('按需加载:');
-		}).catch(error => '加载组件时发生错误!');
+			sayHello('按需加载module.export:');
+		}).catch(error => '加载组件1时发生错误!');
+	}
+	btn2.onclick = () => {
+		import(/* webpackChunkName: "math" */ './math.js').then( module => {
+			var print = module.default;
+			print();
+		}).catch(error => '加载组件2时发生错误!');
+	}
+	btn3.onclick = () => {
+		import(/* webpackChunkName: "math" */ './math.js').then( module => {
+			var cube = module.cube;
+			console.log(cube(2));
+		}).catch(error => '加载组件3时发生错误!');
 	}
 
-	document.body.appendChild(element);
+	document.body.appendChild(btn1);
+	document.body.appendChild(btn2);
+	document.body.appendChild(btn3);
 }
 
 createComponent();
---
+++ src/js/math.js
@@ -0,0 +1,13 @@
+export function square(x) {
+  console.log('squarehaha');
+  return x * x;
+}
+
+export function cube(x) {
+  console.log('cubehaha');
+  return x * x * x;
+}
+
+export default function(){
+  console.log('this is default export');
+}
