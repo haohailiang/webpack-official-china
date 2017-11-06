7a8dfba2f33352570f4bc7e3baeeaa700deb8524
haohailiang <646263409@qq.com>
2017-11-06 16:55:04 +0800

    eslint 语法检查

--- src/js/mainA.js
+++ src/js/mainA.js
@@ -1,5 +1,1 @@
-import component from './moduleA';
-import style1 from './../css/style1.css';
-import style2 from './../css/style2.css';
-
-document.body.appendChild(component('123456',style1.class1,style2.class1));
+console.log('bbc');
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,13 @@
 const path = require('path');
-const webpack = require("webpack");
 const htmlwebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    mainA: './src/js/mainA.js'
+    mainA: './src/js/mainA.js',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: "[name].[hash:5].bundle.js",
+    filename: '[name].[hash:5].bundle.js',
   },
   devServer: {
     host: process.env.HOST, // Defaults to `localhost`
@@ -27,13 +26,13 @@ module.exports = {
             },
           },
         ],
-      }
-    ]
+      },
+    ],
   },
   plugins: [
     //创建了两个HtmlWebpackPlugin的实例，生成两个页面
     new htmlwebpackPlugin({
-      title: 'webpack-dev-server'
-    })
+      title: 'webpack-dev-server',
+    }),
   ],
 };
