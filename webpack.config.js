d9068b8874283a1d78eeaf916c5d69b0764d949e
haohailiang <646263409@qq.com>
2017-11-06 16:01:56 +0800

    webpack-dev-server
    1.指定启动服务的端口号
    2.指定服务的域名

--- webpack.config.js
+++ webpack.config.js
@@ -10,10 +10,14 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: "[name].[hash:5].bundle.js",
   },
+  devServer: {
+    host: process.env.HOST, // Defaults to `localhost`
+    port: 8090, // Defaults to 8080
+  },
   plugins: [
     //创建了两个HtmlWebpackPlugin的实例，生成两个页面
     new htmlwebpackPlugin({
-      title: 'tree-shaking'
+      title: 'webpack-dev-server'
     })
   ],
 };
